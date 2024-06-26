4ff74734c26acc1be99ce9a209305b10b5411954
iboomsakalaka <1305046624@qq.com>
2024-06-26 12:00:46 +0800

    first commit

---
+++ src/customthread.h
@@ -0,0 +1,35 @@
+﻿#ifndef CUSTOMTHREAD_H
+#define CUSTOMTHREAD_H
+
+#include <QThread>
+
+class CustomThread : public QThread
+{
+public:
+    CustomThread();
+    ~CustomThread();
+
+	/**
+	 * @brief	线程运行入口（纯虚函数）
+	 */
+    virtual void run() = 0;
+
+	/**
+	 * @brief	启动线程
+	 * 
+	 * @return	true 成功 false 失败
+	 */
+    bool StartThread();
+
+	 /**
+	 * @brief	停止线程
+	 *
+	 * @return	true 成功 false 失败
+	 */
+    bool StopThread();
+
+protected:
+    bool m_bRunning;    ///< 线程运行标志位
+};
+
+#endif // CUSTOMTHREAD_H
---
+++ src/settingwid.cpp
@@ -0,0 +1,12 @@
+#include "settingwid.h"
+
+SettingWid::SettingWid(QWidget *parent)
+    : QWidget(parent)
+{
+    ui.setupUi(this);
+}
+
+SettingWid::~SettingWid()
+{
+}
+
